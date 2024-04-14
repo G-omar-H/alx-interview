96beb3adb6e73bc6bee765fc869326ec5ec74a16
G-omar-H <omarghazi12@gmail.com>
2024-04-14 17:48:36 +0100

    0. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""
+Pascal triangle technical interview
+"""
+
+
+def pascal_triangle(n):
+    """
+    function that returns a list of a list of lists of integers
+    representing the Pascal's triangle of n
+    Args:
+        n (int): number of rows
+    """
+    if not n:
+        return []
+    rows = []
+    for row in range(n):
+        cols = []
+        for col in range(row + 1):
+            if col == 0:
+                cols.append(1)
+            elif col >= len(rows[row - 1]):
+                cols.append(rows[row - 1][col - 1])
+            else:
+                cols.append(rows[row-1][col - 1] + rows[row - 1][col])
+        rows.append(cols)
+    return rows
