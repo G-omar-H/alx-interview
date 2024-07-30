dafa21f4ca9a06100c83ca3cc6d45b163ed0f159
G-omar-H <omarghazi12@gmail.com>
2024-07-30 15:34:03 +0100

    0. Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -16,7 +16,7 @@ def island_perimeter(grid):
         grid is rectangular, with its width and height not exceeding 100
     The grid is completely surrounded by water
     There is only one island (or nothing).
-    The island doesn't have “lakes” 
+    The island doesn't have “lakes”
     (water inside that isn't connected to the water surrounding the island).
 
 
@@ -28,12 +28,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
-                if not grid[i - 1][j]:
+                if i == 0 or not grid[i - 1][j]:
                     perimeter += 1
-                if not grid[i][j - 1]:
+                if j == 0 or not grid[i][j - 1]:
                     perimeter += 1
-                if not grid[i][j + 1]:
+                if j == len(grid[i]) - 1 or not grid[i][j + 1]:
                     perimeter += 1
-                if not grid[i + 1][j]:
+                if i == len(grid) - 1 or not grid[i + 1][j]:
                     perimeter += 1
     return perimeter
